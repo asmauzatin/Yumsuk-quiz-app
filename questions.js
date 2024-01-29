8e861a4a1ba25ecd98c5fe05db4c5f1105aba5af
asmauzatin <144237776+asmauzatin@users.noreply.github.com>
2024-01-29 07:17:37 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "who is the owner of this application?",
+    answer: "Asmau Hafizu Chiroma",
+    options: [
+      "Suraj Shehu Ibrahim",
+      "Asmau Hafizu Chiroma",
+      "Hafsat Kabir Kulo",
+      "Hauwa Muhammad Sani"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who established Northwest University Kano?",
+    answer: "Rabiu Musa Kwankwaso",
+    options: [
+      "Abba Kabir Yusuf",
+      "Malam Ibrahim Shekarau",
+      "Umar Shafi'u ",
+      "Rabiu Musa Kwankwaso"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What do you call a person who studies computer science?",
+    answer: "Computer scientist",
+    options: [
+      "Computer scientist",
+      "Computer Programer",
+      "Computer engineer",
+      "Computer processor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does BASIC stand for?",
+    answer: "Beginner's All-purpose Symbolic instruction code",
+    options: [
+      "Beginner's All-process Symbolic instruction code",
+      "Beginner's All-purpose Symbolic interface code",
+      "Best All-purpose Symbolic instruction code",
+      "Beginner's All-purpose Symbolic instruction code"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does GIGO stand for?",
+    answer: "Garbage in Garbage out",
+    options: [
+      "get in get out",
+      "Generation in generation out",
+      "graphics in graphics out",
+      "Garbage in Garbage out"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
